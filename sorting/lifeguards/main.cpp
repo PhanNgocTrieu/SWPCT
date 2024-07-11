d1a5710e0c592bf020e217b6152858b77caa50fe
Trieu John <pntrieu200799@gmail.com>
2024-07-11 13:39:31 +0700

    Create main.cpp

---
+++ sorting/lifeguards/main.cpp
@@ -0,0 +1,133 @@
+/*
+    Links: https://usaco.org/index.php?page=viewproblem2&cpid=715
+*/
+
+#include <iostream>
+#include <bits/stdc++.h>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+#define inf 0x3f3f3f3f
+#define infll 0x3f3f3f3f3f3f3f
+#define ll long long
+
+using s = std::string;
+using vll = vector<ll>;
+using vb = vector<bool>;
+using mll = map<ll,ll>;
+using umll = unordered_map<ll,ll>;
+using pll = pair<ll,ll>;
+using pls = pair<ll,s>;
+using vpll = vector<pair<ll,ll>>;
+
+// loop templates
+#define F_OR(i, a, b, s) for (ll i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e , 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
+#define EACH(x, a) for (auto& x : a)
+#define GET5(a, b, c, d, e, ...) e
+#define F_ORC(...) GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1)
+
+#define loop_1(s) for (ll i = 0; i < s; ++i)
+#define loop_se(st, en) for (ll i = st; i <= en; ++i)
+
+#define loop(...) F_ORC(__VA_ARGS__)(__VA_ARGS__)
+
+#define p_m(m) \
+    for (auto v : m) \
+        cout << "(" << v.first << ", " << v.second  << ")" << endl;
+
+// debug templates
+#define debug(x) cout << #x << ": " << x << endl;
+#define debugas(a,n) for (int i = 0; i < n; ++i) { cout << #a << "[i]: " << a[i] << " "; } cout << endl;
+#define debugv(v) cout << #v << "[ "; for (int i = 0; i < (v).size(); ++i) cout << v[i] << " "; cout << "]" << endl;
+#define debugm(m) cout << #m << " = [ " << endl; EACH(x, m) { cout << "(" << x.first << ", " << x.second << ")" << endl;} cout << "]" << endl;
+#define debugmp(m) cout << #m << " = [ " << endl; EACH(x, m) { cout << "(" << x.first << ", {" << x.second.first << ", " << x.second.second << "})" << endl;} cout << "]" << endl;
+#define debugia(a, f, s) cout << "(" << f << "," << s << "): (" << a[f] << ", " << a[s] << ")" << endl;
+#define debugvp(v) cout << #v <<  " [" << endl; EACH(x, v) { cout << " (" << x.first << ", " << x.second << ") " << endl; } cout << "] " << endl;
+#define debugvtp(v) cout << #v << "[ " << endl; EACH(e, v) { cout << "(" << get<0>(e) << ", " << get<1>(e) << ", " << get<2>(e) << ")" << endl; } cout << "]" << endl;
+#define debugset(s) cout << #s << "[ "; EACH(e, s) { cout << e << " "; } cout << "] " << endl;
+
+#define READ_FILE
+// #define TESTCASE
+
+void solve() {
+    ll ans = 1;
+    ll N; cin >> N;
+    
+    vector<tuple<ll,ll,bool>> events;
+
+    loop(N) {
+        ll in, out;
+        cin >> in >> out;
+        events.push_back(std::make_tuple(in, i, true));
+        events.push_back(std::make_tuple(out,i, false));
+    }
+
+    sort(events.begin(), events.end());
+
+    // debugvtp(events);
+
+    set<ll> actives;
+    vll alone_time(N);
+    ll prev_time = 0;
+    ll total_time = 0;
+
+    EACH(e, events) {
+        // get time
+        ll cur_time = get<0>(e);
+        
+        if (actives.size() > 0) {
+            total_time += cur_time - prev_time;
+        }
+
+        if (actives.size() == 1) {
+            alone_time[*actives.begin()] += cur_time - prev_time;
+        }
+
+        if (get<2>(e)) {
+            actives.insert(get<1>(e));
+        } else {
+            actives.erase(get<1>(e));
+        }
+
+        prev_time = cur_time;
+
+        // cout << "prev: " << prev_time << " - cur: " << cur_time << endl;
+        // debugv(alone_time);
+        // debugset(actives);
+    }
+    ll min_alone_time = *min_element(alone_time.begin(), alone_time.end());
+    cout << total_time - min_alone_time << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifdef READ_FILE
+    FILE* f_in = freopen("lifeguards.in", "r", stdin);
+    FILE* f_out = freopen("lifeguards.out", "w", stdout);
+#endif
+
+#ifdef TESTCASE
+    ll ts;
+    cin >> ts;
+    while (ts--) {
+        solve();
+    }
+#else
+    solve();
+#endif
+
+#ifdef READ_FILE
+    fclose(f_in);
+    fclose(f_out);
+#endif
+    return 0;
+}
