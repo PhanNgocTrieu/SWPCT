c2f716b72f40870193264376f20b8d38bc86a053
TRIEU NGOC PHAN/LGEDV MULTIMEDIA TEAM(trieu.phan@lge.com) <trieu.phan@lge.com>
2024-09-26 09:02:36 +0700

    update

--- leetcode/729.cpp
+++ leetcode/729.cpp
@@ -1,15 +1,33 @@
 // https://leetcode.com/problems/my-calendar-i/description/?envType=daily-question&envId=2024-09-26
 #include <bits/stdc++.h>
+#include <vector>
 using namespace std;
+
+#define max_size (unsigned long long)1e9
+
 class MyCalendar {
-    map<long long, long long> m_calendar;
+    vector<long long> starts{max_size, 0};
+    vector<long long> ends{max_size, 0};
 public:
     MyCalendar() {
-        
+        // cout << "starts[2]: " << starts[2] << endl;
     }
     
     bool book(int start, int end) {
-        
+        if (starts.empty()) {
+            starts[start] = 1;
+            ends[start] = 1;
+        }
+        auto get = lower_bound(starts.begin(), starts.end(), start);
+        // [this](int a, int b){
+        //     return starts[a] == 1;
+        // }
+        if (get == starts.end()) {
+            cout << "not found!" << endl;
+        }
+        else {
+            cout << "found: " << *get << endl;
+        }
     }
 };
 
@@ -21,7 +39,8 @@ public:
 
 int main() {
     MyCalendar * schedule = new MyCalendar();
-    schedule->book(0, 2);
+    schedule->book(3, 5);
+    schedule->book(4, 2);
     if (schedule) {
         delete schedule;
         schedule = nullptr;
