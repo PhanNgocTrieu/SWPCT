22a553313a8768009919deffabea344d20bd24f6
TRIEU NGOC PHAN/LGEDV MULTIMEDIA TEAM(trieu.phan@lge.com) <trieu.phan@lge.com>
2024-11-20 10:02:37 +0700

    updated

--- main.cpp
+++ main.cpp
@@ -1,108 +1,124 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
-#define ll long long
+
 #define MAX_RC	(100)
 
 int R, C;
-int xS, yS;
 char map[MAX_RC][MAX_RC + 1];
-
-void bfs() {
-
-}
-
+int dx[] = {-1 ,1, 0, 0};
+int dy[] = {0, 0, -1 ,1};
+int sX = 0;
+int sY = 0;
+int ans = 0;
+struct Pos{
+	int x = 0;
+	int y = 0;
+	int l = 0;
+	Pos(int x, int y, int l) : x(x), y(y), l(l) {}
+};
+// vector<bool> numbers(10, false);
 void Input_Data(void) {
 	cin >> R >> C;
-    bool found = false;
 	for (int r = 0; r < R; r++) {
 		cin >> map[r];
-        if (!found) {
-            for (int i = 0; i < C; ++i) {
-                if (map[r][i] == 'S') {
-                    xS = r;
-                    yS = i;
-                }
-            }
-        }
 	}
 }
 
-class Solution {
-public:
-    vector<int> resultsArray(vector<int>& nums, int k) {
-		vector<int> ans;
-		int l = 0;
-		int r = 0;
-		int s = nums.size();
-		int z = 1;
-		for (int i = 1; i < s; ++i) {
-			cout << "nums: " << nums[i] << endl;
-			if (z == k) { 
-				r = i;
-				break; 
+void findS() {
+	for (int i = 0; i < R; ++i) {
+		for (int j = 0; j < C; ++j) {
+			if (map[i][j] == 'S') {
+				sX = i;
+				sY = j;
+				return;
 			}
-			if (nums[i] >= nums[i - 1]) {
-				cout << __LINE__ << endl;
-				l++;
-				z = 1;
-				ans.push_back(-1);
+		}
+	}
+}
+
+void bfs() {
+	bool numbers[MAX_RC][MAX_RC + 1];
+	bool foundNumber = false;
+	bool endPoint = false;
+	Pos* v = new Pos({sX, sY, 0});
+	do {
+		if (v == nullptr) {
+			break;
+		}
+		cout << "work at: (x,y): " << v->x << ", " << v->y << '\n';
+		foundNumber = false;
+		int disMin = INT_MAX;
+		queue<Pos> q;
+		q.push(*v);
+		cout << "ans += " << v->l << " -> ans = " << ans << '\n';
+		ans += v->l;
+		{
+			if (!endPoint) {
+				numbers[v->x][v->y] = true;
 			}
-			else {
-				z++;
+			delete v;
+			v = nullptr;
+		}
+		bool visited[MAX_RC][MAX_RC + 1];
+		while (!q.empty()) {
+			auto p = q.front();
+			q.pop();
+			visited[p.x][p.y] = true;
+			cout << " p of (x,y): " << p.x << "," << p.y << '\n';
+			for (int i = 0; i < 4; ++i) {
+				int nx = p.x + dx[i];
+				int ny = p.y + dy[i];
+				int nl = p.l + 1;
+				if (
+					nx >= 0 && nx < R &&
+					ny >= 0 && ny < C &&
+					map[nx][ny] != '*' &&
+					visited[nx][ny] == false &&
+					numbers[nx][ny] == false
+				) {
+					if (endPoint) {
+						if (map[nx][ny] == 'S') {
+							if (ans > nl) {
+								ans += nl;
+							}
+						}
+					}
+					else {
+						visited[nx][ny] = true;
+						if (map[nx][ny] != '.') {
+							foundNumber = true;
+							if (v == nullptr)
+								v = new Pos({nx, ny, nl});
+							else {
+								if (nl < disMin) {
+									v->x = nx;
+									v->y = ny;
+									v->l = nl;
+									disMin = nl;
+								}
+							}
+						}
+					}
+					q.push({nx, ny, nl});
+				}
 			}
 		}
 
-		// for (int i = l; i < s; ++i) {
-
-		// }
-
-		for (auto a : ans) {
-			cout << a << " ";
+		if (!foundNumber) {
+			endPoint = true;
 		}
-		cout << endl;
 
-		return ans;
-    }
-};
+	} while (foundNumber || endPoint);
 
-class Solution {
-public:
-    std::vector<int> resultsArray(std::vector<int>& nums, int k) {
-        // Skip if k is 1
-        if (k == 1) {
-            return nums;
-        }
-        
-        int n = nums.size();
-        std::vector<int> result;
-        int left = 0;
-        int right = 1;
-        
-        while (right < n) {
-            // Check if current sequence is not consecutive
-            bool is_not_consecutive = nums[right] - nums[right-1] != 1;
-            
-            if (is_not_consecutive) {
-                // Mark invalid sequences
-                while (left < right && left + k - 1 < n) {
-                    result.push_back(-1);
-                    left++;
-                }
-                left = right;
-            }
-            // Found valid k-length sequence
-            else if (right - left == k - 1) {
-                result.push_back(nums[right]);
-                left++;
-            }
-            
-            right++;
-        }
-        
-        return result;
-    }
-};
+}
+
+int solve() {
+	findS();
+	bfs();
+	return ans;
+}
 
 int main(void) {
 	ios_base::sync_with_stdio(false);
@@ -112,16 +128,12 @@ int main(void) {
 	int sol = -1;
 
 	// 입력 받는 부분
-	// Input_Data();
-
-	vector<int> nums = {2,2,2,2,2};
-	Solution{}.resultsArray(nums, 4);
-
+	Input_Data();
 
 	// 여기서부터 작성
-
+	sol = solve();
 	// 출력 하는 부분
-	// cout << sol << '\n';
+	cout << sol << '\n';
 
 	return 0;
 }
