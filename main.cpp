b748a81dc27f27db7ecdb6cb09084538c0f3b2cb
TRIEU NGOC PHAN/LGEDV MULTIMEDIA TEAM(trieu.phan@lge.com) <trieu.phan@lge.com>
2024-11-21 09:21:17 +0700

    updated

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int dx[] = {-1 ,1, 0, 0};
 int dy[] = {0, 0, -1 ,1};
 int sX = 0;
 int sY = 0;
-int ans = 0;
+int ans = INT_MAX;
 struct Pos{
 	int x = 0;
 	int y = 0;
@@ -23,100 +23,32 @@ void Input_Data(void) {
 	cin >> R >> C;
 	for (int r = 0; r < R; r++) {
 		cin >> map[r];
-	}
-}
-
-void findS() {
-	for (int i = 0; i < R; ++i) {
-		for (int j = 0; j < C; ++j) {
-			if (map[i][j] == 'S') {
-				sX = i;
-				sY = j;
-				return;
+		for (int c = 0; c < C; ++c) {
+			if (map[r][c] == 'S') {
+				sX = r;
+				sY = c;
 			}
 		}
 	}
 }
 
-void bfs() {
-	bool numbers[MAX_RC][MAX_RC + 1];
-	bool foundNumber = false;
-	bool endPoint = false;
-	Pos* v = new Pos({sX, sY, 0});
-	do {
-		if (v == nullptr) {
-			break;
-		}
-		cout << "work at: (x,y): " << v->x << ", " << v->y << '\n';
-		foundNumber = false;
-		int disMin = INT_MAX;
-		queue<Pos> q;
-		q.push(*v);
-		cout << "ans += " << v->l << " -> ans = " << ans << '\n';
-		ans += v->l;
-		{
-			if (!endPoint) {
-				numbers[v->x][v->y] = true;
-			}
-			delete v;
-			v = nullptr;
-		}
-		bool visited[MAX_RC][MAX_RC + 1];
-		while (!q.empty()) {
-			auto p = q.front();
-			q.pop();
-			visited[p.x][p.y] = true;
-			cout << " p of (x,y): " << p.x << "," << p.y << '\n';
-			for (int i = 0; i < 4; ++i) {
-				int nx = p.x + dx[i];
-				int ny = p.y + dy[i];
-				int nl = p.l + 1;
-				if (
-					nx >= 0 && nx < R &&
-					ny >= 0 && ny < C &&
-					map[nx][ny] != '*' &&
-					visited[nx][ny] == false &&
-					numbers[nx][ny] == false
-				) {
-					if (endPoint) {
-						if (map[nx][ny] == 'S') {
-							if (ans > nl) {
-								ans += nl;
-							}
-						}
-					}
-					else {
-						visited[nx][ny] = true;
-						if (map[nx][ny] != '.') {
-							foundNumber = true;
-							if (v == nullptr)
-								v = new Pos({nx, ny, nl});
-							else {
-								if (nl < disMin) {
-									v->x = nx;
-									v->y = ny;
-									v->l = nl;
-									disMin = nl;
-								}
-							}
-						}
-					}
-					q.push({nx, ny, nl});
-				}
-			}
-		}
+int backtrack(vector<vector<bool>> visited, int x, int y, int dis) {
+	if (x < 0 && x >= R
+	&& y < 0 && y >= C
+	&& visited[x][y]
+	) {
+		return dis;
+	}
 
-		if (!foundNumber) {
-			endPoint = true;
-		}
-
-	} while (foundNumber || endPoint);
+	visited[x][y] = true;
+	auto r = backtrack(visited, x, y + 1, dis + 1);
 
 }
 
 int solve() {
-	findS();
-	bfs();
+	vector<vector<bool>> visited(R, vector<bool>(C,false));
+	ans = min(ans, backtrack(visited, sX, sY, 0));
+
 	return ans;
 }
 
