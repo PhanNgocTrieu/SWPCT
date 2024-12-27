d5fdd1812c64ca5459eba9596aa2598c015a01ba
Trieu John <pntrieu200799@gmail.com>
2024-12-28 00:50:27 +0700

    Create 3392.cpp

---
+++ leetcode/3392.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int countSubarrays(vector<int>& nums) {
+        int ans = 0;
+        for (int i = 2; i < nums.size() ; ++i) {
+            ans += ((nums[i - 2] + nums[i]) * 2) == (nums[i - 1]);
+        }
+        return ans;
+    }
+};
