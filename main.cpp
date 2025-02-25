23eac2546ea49f495d5b6f50461bb22b4919d2c8
TRIEU NGOC PHAN/LGEDV MULTIMEDIA TEAM(trieu.phan@lge.com) <trieu.phan@lge.com>
2025-02-25 15:29:00 +0700

    upload files

--- main.cpp
+++ main.cpp
@@ -4,24 +4,25 @@ using namespace std;
 #define ll long long
 
 void solve() {
-    ll n, t;
-    cin >> n >> t;
-    vector<ll> a(1e5, -1);
-    for (int i = 0; i < n; i++) {
-        ll x; cin >> x;
-        if (a[x] != -1) {
-            cout << a[x] << " " << i+1 << '\n';
-            return;
-        }
-        a[t-x] = i + 1;
+    ll x,y;
+    cin >> x >> y;
+
+    if (y == x + 1) {
+        cout << "YES" << endl;
     }
-    cout << "IMPOSSIBLE" << endl;
+    else if (x > y && (x - y + 1) % 9 == 0) {
+        cout << "YES" << endl;
+    }
+    else cout << "NO" << endl;
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
-    solve();
+    ll t; cin >> t;
+    while(t--) {
+        solve();
+    }
     return 0;
 }
