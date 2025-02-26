b059ec8a9f99b0f67322bf92988e12a130aec40c
TRIEU NGOC PHAN/LGEDV MULTIMEDIA TEAM(trieu.phan@lge.com) <trieu.phan@lge.com>
2025-02-26 17:53:28 +0700

    update valid BFS need to improve

--- main.cpp
+++ main.cpp
@@ -1,22 +1,129 @@
 // https://vjudge.net/contest/695101#problem/B
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
+#define debug_q(q) \
+    { \
+        auto q_ = q; \
+        while (!q_.empty()) { \
+            cout << q_.front() << ' '; \
+            q_.pop(); \
+        } \
+        cout << '\n'; \
+    }
+
+#define debug_v(v) \
+    { \
+        for (auto x : v) { \
+            cout << x << ' '; \
+        } \
+        cout << '\n'; \
+    }
+
+#define debug_vv(vv) \
+    { \
+        for (int i = 0; i < vv.size(); i++) { \
+            cout << "v[" << i << "]: "; \
+            for (auto x : vv[i]) { \
+                cout << x << ' '; \
+            } \
+            cout << '\n'; \
+        } \
+    }
+
+#define debug_x(x) \
+    { \
+        cout << #x << ": " << x << '\n'; \
+    }
+
 typedef long long ll;
+ll n;
+queue<ll> sq;
+ll start = 1;
+bool checkBFS(const vector<vector<ll>>& a) {
+    if (sq.front() != start) {
+        return false;
+    }
+
+    queue<ll> q;
+    vector<bool> visited(n + 1, false);
+    vector<bool> s(n + 1, false);
+    q.push(start);
+    visited[start] = true;
+    ll cur_s = 0;
+    s[start] = true;
+    sq.pop();
+
+    while(!q.empty()) {
+        auto cur_u = q.front();
+        // debug_x(q.front());
+        q.pop();
+
+        if (cur_u == -1) {
+            // debug_x(cur_s);
+            while (cur_s > 0)
+            {
+                cur_s--;
+                if (sq.empty()) return false;
+                // debug_x(cur_s);
+                auto x = sq.front();
+                if (s[x] == false) {
+                    return false;
+                }
+                sq.pop();
+                s[x] = false;
+            }
+        }
+        else {
+            q.push(-1);
+            for (auto x : a[cur_u]) {
+                s[x] = true;
+                cur_s++;
+                // debug_x(x);
+                if (!visited[x]) {
+                    visited[x] = true;
+                    q.push(x);
+                }
+            }
+        }
+    }
+
+    return sq.empty() ? true : false;
+}
 
 void solve() {
+    cin >> n;
+    vector<vector<ll>> a(n + 1);
+    for (int i = 0; i < n - 1; i++) {
+        ll u, v; cin >> u >> v;
+        a[u].push_back(v);
+        if (i == 0) {
+            start = u;
+        }
+    }
 
+    // debug_vv(a);
+
+    for (int i = 0; i < n; i++) {
+        ll x; cin >> x;
+        sq.push(x);
+    }
+
+    if (checkBFS(a)) {
+        cout << "YES" << '\n';
+    }
+    else {
+        cout << "NO" << '\n';
+    }
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    ll t; cin >> t;
-    while (t--) {
+    // ll t; cin >> t;
+    // while (t--) {
         solve();
-    }
+    // }
     return 0;
 }
