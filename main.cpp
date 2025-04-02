45615358a9a023dd4930df49ca1ee351ea1f07c5
TRIEU NGOC PHAN/LGEDV MULTIMEDIA TEAM(trieu.phan@lge.com) <trieu.phan@lge.com>
2025-04-02 10:07:05 +0700

    update files

--- main.cpp
+++ main.cpp
@@ -13,25 +13,36 @@ void setIO(const string& file) {
 void solve() {
     ll n, m, k, d;
     cin >> n >> m >> k >> d;
-    multiset<pair<ll, ll>> s;
     vector<vector<ll>> rivers(n + 1, vector<ll>(m + 1));
+    vector<ll> s_s;
     for (ll i = 0; i < n; i++) {
-        for (ll j = 0; j < m; j++) {
+        for (ll j = 1; j < m; j++) {
             cin >> rivers[i][j];
         }
     }
 
 
-    for (int i = 0; i < n; ++i) {
+    for (ll i = 0; i < n; ++i) {
+        multiset<ll> s;
         vector<ll> dp(m);
-        for (int j = 1; j < m; ++j) {
+        ll min_val = INT_MAX;
+        dp[0] = 1;
+        for (ll j = 1; j < m; ++j) {
+            if (s.size() >= d) {
+                s.erase(s.find(rivers[i][j - d]));
+            }
 
+            dp[j] = *s.begin() + 1 + rivers[i][j];
+            s.insert(rivers[i][j] + 1);
         }
     }
+
+
+
 }
 
 int main() {
-    setIO("circlecross");
+    // setIO("circlecross");
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
